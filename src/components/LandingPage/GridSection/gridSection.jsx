3e25c148382d57e1de57f47c0f1a31eac5bb1965
anataku ameerah <anatakuameerah@gmail.com>
2024-09-01 19:08:24 +0100

    grid-adjustment and code tweaks

--- src/components/LandingPage/GridSection/gridSection.jsx
+++ src/components/LandingPage/GridSection/gridSection.jsx
@@ -7,10 +7,10 @@ function GridSection() {
                 <h3 className="feel">Feel the best experience <br /> with our features</h3>
                 <div className="grid">
                     <div className="grid-box">
-                       <div className="grid-icons" ><img className="" src="/shopping-bag.svg" alt="shopping-bag" /></div> 
+                        <div className="grid-icons" ><img className="" src="/shopping-bag.svg" alt="shopping-bag" /></div>
                         <h5 className="seemless-shopping"> Seamless Shopping</h5>
-                        <p  className="grid-box-p">CampusDash is your one-stop shop for all things campus. Whether you need textbooks, snacks, tech gadgets, or dorm supplies, we've got it all.</p>
-                       <div className="grid-icon"><img className="mockup" src="/Mockup (1).svg" alt="mockup" /></div> 
+                        <p className="grid-box-p">CampusDash is your one-stop shop for all things campus. Whether you need textbooks, snacks, tech gadgets, or dorm supplies, we've got it all.</p>
+                        <div className="grid-icon"><img className="mockup" src="/Mockup (1).svg" alt="mockup" /></div>
                     </div>
                     <div className="grid-box">
                         <div className="grid-icons">  <img src="/people.svg" alt="people" /></div>
@@ -19,7 +19,7 @@ function GridSection() {
                         <div className="grid-icon"><img className="avatar" src="/Frame 1171274842.svg" alt="" /></div>
                     </div>
                     <div className="grid-box">
-                    <div className="grid-icons"><img src="/wallet-money.svg" alt="wallet" /></div>
+                        <div className="grid-icons"><img src="/wallet-money.svg" alt="wallet" /></div>
                         <h5 className="seemless-shopping">Easy Payments</h5>
                         <p className="grid-box-p" >Our payment gateway is encrypted and secure, ensuring that your transactions are safe. Plus, with our simple checkout process, you can complete your purchase in just a few taps.</p>
                         <div>
@@ -28,14 +28,14 @@ function GridSection() {
                         </div>
                     </div>
                     <div className="grid-box">
-                    <div className="grid-icons"><img src="/gift.svg" alt="gift" /></div>
+                        <div className="grid-icons"><img src="/gift.svg" alt="gift" /></div>
                         <h5 className="seemless-shopping">Dash Rewards</h5>
                         <p className="grid-box-p">CampusDash thrives on community, and we believe in rewarding our users for helping us grow. With our referral program, you can earn exclusive rewards by inviting your friends to join CampusDash. Every time a friend signs up using your referral code and makes a purchase, you’ll earn points that can be redeemed for discounts, special offers, and more. The more friends you refer, the bigger the rewards</p>
                         <div className="grid-icon"><img className="reward-card" src="/Frame 109.svg" alt="" /></div>
                     </div>
                 </div>
                 <div className="get-the">
-                <h6 className="get-the-app" >Get the Mobile app.</h6>
+                    <h6 className="get-the-app" >Get the Mobile app.</h6>
                     <p>Kindly get notified when the app is released by joining <br /> our waitlist</p>
                     <div>
                         <input className="grid-email-2" type="email" placeholder="name@gmail.com" name="" id="" />
